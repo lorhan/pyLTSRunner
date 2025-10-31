a328e6e4297801f0a72cefcbdcae56d5c768d8ce
Lorhan Costa <v.lorhan@gmail.com>
2025-10-31 22:09:17 +0100

    now reading inputs from a json file and using schema-validation

--- pyLTSRunner.py
+++ pyLTSRunner.py
@@ -2,72 +2,106 @@ from PyLTSpice import SimRunner, SpiceEditor, LTspice, RawRead
 import matplotlib.pyplot as plt
 import numpy as np
 
+import json
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
 
 
 
-NR_CORES = 2
-DIR_OUTPUT_RAW = "temp_batch3"
-DIR_STORAGE_NPZ = "results_in_npz"
-PATH_SPICE_MODEL = "LTSPiceModel/Draft1.asc"
+def get_validated_json_data(filepath_json):
+
+    schema = {
+        "type": "object",
+        "properties": {
+            "PATH_SPICE_MODEL": {"type": "string"},
+            "DIR_OUTPUT_RAW": {"type": "string"},
+            "DIR_STORAGE_NPZ": {"type": "string"},
+            "PRL_SIMS": {"type": "integer", "minimum": 1},
+        },
+        "required": ["PATH_SPICE_MODEL", "DIR_OUTPUT_RAW", "DIR_STORAGE_NPZ", "PRL_SIMS"]
+    }
+    # Read JSON data from file
+    with open(filepath_json, "r") as f:
+        jdata = json.load(f)
+
+    # Validate
+    try:
+        validate(instance=jdata, schema=schema)
+        print("JSON is valid!")
+    except ValidationError as e:
+        exception_message = f"JSON validation error:{e.message}"
+        raise Exception(exception_message)
+
+    return jdata
+
+
 
 def processing_data(raw_file, log_file):
     """This is the function that will process the data from simulations"""
     print("Handling the simulation data of %s, log file %s" % (raw_file, log_file))
 
-# Configures the simulator to use and output folder. Also defines the number of parallel simulations
-runner = SimRunner(output_folder=f'./{DIR_OUTPUT_RAW}', simulator=LTspice, parallel_sims=NR_CORES)
-
-netlist = SpiceEditor(PATH_SPICE_MODEL)  # Open the Spice Model, and creates the .net
-# set default arguments
-netlist.set_component_value('R1', '5')  # Modifying the value of a resistor
-
-liResValues = [f"{n}" for n in [1,2,3,4]]
 
 
-for res_value in liResValues:
-    netlist.set_component_value('R1', res_value)
-    # overriding he automatic netlist naming
-    run_netlist_file = f"R1_{res_value}.net"
-    # This will launch up to 'parallel_sims' simulations in background before waiting for resources
-    runner.run(netlist, run_filename=run_netlist_file, callback=processing_data)
-
-# This will wait for the all the simulations launched before to complete.
-runner.wait_completion()
-# The timeout counter is reset everytime a simulation is finished.
-
-# Sim Statistics
-print('Successful/Total Simulations: ' + str(runner.okSim) + '/' + str(runner.runno))
+if __name__ == "__main__":
 
 
-diUouts = {}
-for resval in liResValues:
-    raw = RawRead(f"{DIR_OUTPUT_RAW}/R1_{resval}.raw")
-    time = raw.get_axis()
-    Uout = raw.get_trace('V(Uout)').get_wave()
-    diUouts[resval] = Uout
+    filepath_json = "myJson.json"
+    jData = get_validated_json_data(filepath_json)
 
-    np.savez_compressed(
-        f"{DIR_STORAGE_NPZ}/R1_{resval}.npz",
-        time=time,
-        Uout=Uout,
-        description=f"Data for R1_{resval} dadas")
+    # Configures the simulator to use and output folder. Also defines the number of parallel simulations
+    runner = SimRunner(output_folder=f'./{jData["DIR_OUTPUT_RAW"]}', simulator=LTspice, parallel_sims=jData["PRL_SIMS"])
 
+    netlist = SpiceEditor(jData["PATH_SPICE_MODEL"])  # Open the Spice Model, and creates the .net
+    # set default arguments
+    netlist.set_component_value('R1', '5')  # Modifying the value of a resistor
 
-    plt.plot(time, Uout,label = f"R1={resval}")
-
-for n in diUouts.keys():
-    print(f"R1={n}: {len(diUouts[n])} values")
-# breakpoint()
+    liResValues = [f"{n}" for n in [1,2,3,4]]
 
 
+    for res_value in liResValues:
+        netlist.set_component_value('R1', res_value)
+        # overriding he automatic netlist naming
+        run_netlist_file = f"R1_{res_value}.net"
+        # This will launch up to 'parallel_sims' simulations in background before waiting for resources
+        runner.run(netlist, run_filename=run_netlist_file, callback=processing_data)
+
+    # This will wait for the all the simulations launched before to complete.
+    runner.wait_completion()
+    # The timeout counter is reset everytime a simulation is finished.
+
+    # Sim Statistics
+    print('Successful/Total Simulations: ' + str(runner.okSim) + '/' + str(runner.runno))
+
+
+    diUouts = {}
+    for resval in liResValues:
+        raw = RawRead(f"{jData['DIR_OUTPUT_RAW']}/R1_{resval}.raw")
+        time = raw.get_axis()
+        Uout = raw.get_trace('V(Uout)').get_wave()
+        diUouts[resval] = Uout
+
+        np.savez_compressed(
+            f"{jData['DIR_STORAGE_NPZ']}/R1_{resval}.npz",
+            time=time,
+            Uout=Uout,
+            description=f"Data for R1_{resval} dadas")
+
+
+        plt.plot(time, Uout,label = f"R1={resval}")
+
+    for n in diUouts.keys():
+        print(f"R1={n}: {len(diUouts[n])} values")
+    # breakpoint()
 
 
 
 
-plt.legend()
-plt.grid()
-plt.show()
 
 
-myRecData = data = np.load(f"{DIR_STORAGE_NPZ}/R1_{liResValues[1]}.npz", allow_pickle=True)
-breakpoint()
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+
+    myRecData = data = np.load(f"{jData['DIR_STORAGE_NPZ']}/R1_{liResValues[1]}.npz", allow_pickle=True)
+    breakpoint()
